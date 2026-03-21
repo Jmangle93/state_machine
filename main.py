bc4cec3cd5c3ea8cfd02c57577ccca484a6c69e7
John Michael Angle <jmangle93@gmail.com>
2026-03-21 13:08:55 -0600

    Set main.py to empty default with examples commented

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ from vision_service import VisionService
 from input_service import InputService
 from context import Context
 from state_machine import StateMachine
-from states import (
-    CheckInventoryOpen,
-    OpenInventory,
-    CheckBanker,
-    BankBanker
-)
-from barbarian_fishing_states import (
-    CheckInventory,
-    MakeBait,
-    CheckSpot,
-    Fish
-)
+# from states import (
+#     SomeState,
+#     StartState
+# )
 
 vision = VisionService(threshold=0.80)
 input_service = InputService()
@@ -21,15 +13,8 @@ context = Context(vision, input_service)
 
 state_machine = StateMachine(context)
 
-# state_machine.add_state(CheckInventoryOpen("CheckInventoryOpen"))
-# state_machine.add_state(OpenInventory("OpenInventory"))
-# state_machine.add_state(CheckBanker("CheckBanker"))
-# state_machine.add_state(BankBanker("BankBanker"))
+# state_machine.add_state(SomeState("SomeState"))
+# state_machine.add_state(OpenInventory("StartState"))
 
-state_machine.add_state(CheckInventory("CheckInventory"))
-state_machine.add_state(MakeBait("MakeBait"))
-state_machine.add_state(CheckSpot("CheckSpot"))
-state_machine.add_state(Fish("Fish"))
-
-state_machine.set_start("CheckInventory")
+# state_machine.set_start("StartState")
 state_machine.run()
